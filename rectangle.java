7a291d4bd1bdea1d0860c6a159b56887571c5f02
12kajalkumari <108114263+12kajalkumari@users.noreply.github.com>
2022-06-24 20:35:13 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,29 @@
+import java.util.*;
+ class test
+{
+	public static void main(String[] args)
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		int b=sc.nextInt();
+		int h=sc.nextInt();
+		System.out.println("enter a breadth:"+b);
+		System.out.println("enter a height:"+h);
+		
+		int area=b*h;
+		System.out.println("area of rectangle="+area);
+		
+	
+	
+	}
+}
+	
+		  
+		  
+		
+		  
+	  
+	
+
+		
+		
