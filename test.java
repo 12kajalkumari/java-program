442d8c891aeec2a4eb5e166a0e2c96fca6bfe74e
12kajalkumari <108114263+12kajalkumari@users.noreply.github.com>
2022-06-24 20:38:23 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ class Area
+{
+	public static void main(String[] args)
+	{
+		double pI=3.14,area;
+		int r;
+		Scanner sc=new Scanner(System.in);
+		r=sc.nextInt();
+		area=pI*r*r;
+		System.out.println("area of circle:"+area);
+	}
+}
+	
+		  
+		  
+		
+		  
+	  
+	
+
+		
+		
