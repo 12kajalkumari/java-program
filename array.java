e3c6d47cc76e0b115813d8a5280d891e5f2e9b88
12kajalkumari <108114263+12kajalkumari@users.noreply.github.com>
2022-06-24 11:00:39 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+ class test
+  {
+
+   public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+for(int i=2;i<num;i++){
+	if(num%i==0){
+		System.out.println("prime");
+	}
+	else{
+		System.out.println("not prime");
+	}
+}
+}
+}
+		
+		
+
+	
+	
+
+	
+
+
+
