6600f98e3ca43d0c957f580bddf8faee8e2b712e
12kajalkumari <108114263+12kajalkumari@users.noreply.github.com>
2022-06-25 15:22:29 +0530

    Add files via upload

---
+++ kajal4.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class test
+{
+public static void main(String[] args)
+{
+	Scanner sc=new Scanner(String.in);
+	System.out.println("enter your number");
+	int num=sc.nextint();
+	int total=0;
+	int store=num;
+	while(num>0)
+	{
+		int rem=num%10;
+		total=total+(rem*rem*rem);
+	    num=num/10;
+	}
+	System.out.println(total);
+	if(totel==store)
+	{
+		System.out.println("armstrong");
+	}
+	else{
+		System.out.println(" not armstrong");
+	}
+}
+	
+		
+}
