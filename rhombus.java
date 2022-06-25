3df61044b5bcc68c573985005abdc37b320ccded
12kajalkumari <108114263+12kajalkumari@users.noreply.github.com>
2022-06-25 15:21:40 +0530

    Add files via upload

---
+++ rhombus.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class area {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		double d1=sc.nextDouble();
+		double d2=sc.nextDouble();
+		System.out.println("enter your number:"+d1);
+		System.out.println("enter your number:"+d2);
+		
+		double area=(d1*d2)/2;
+		System.out.println("area of rhombus:"+area);
+	}
+}
+		
+		
+		
+		
+		
+		
+		
